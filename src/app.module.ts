5c116a74123e9a5cd6581b6d3afde0826cee81e4
suemor <3100825062@qq.com>
2022-03-07 21:52:18 +0800

    feat: 构建数据库

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+
 @Module({
   imports: [],
   controllers: [AppController],
